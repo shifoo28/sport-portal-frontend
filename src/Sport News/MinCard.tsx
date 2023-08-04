32617f39f0270c6b9230b2e3575ef24e9c9719f9
shifoo28 <140791885+shifoo28@users.noreply.github.com>
2023-08-04 19:03:38 +0500

    Add files via upload

---
+++ src/Sport News/MinCard.tsx
@@ -0,0 +1,27 @@
+import { Typography } from "@material-tailwind/react";
+import React, { FC } from "react";
+
+type Props = {
+  imgLink: string;
+  title: string;
+};
+
+const MinCard: FC<Props> = ({ imgLink, title }) => {
+  return (
+    <figure className={`relative h-[131px] w-[200px]`}>
+      <img
+        className="h-full w-full  object-cover object-center"
+        src={imgLink}
+        alt="nature image"
+      />
+      <div className="absolute inset-0 m-0 bg-gradient-to-t from-black/60 via-black/50 " />
+      <figcaption className="absolute text-white bottom-4 left-0 w-full max-w-[356px] cursor-pointer ml-2">
+        <Typography className="font-sofiasans text-[10px] max-w-[110px]">
+          {title}
+        </Typography>
+      </figcaption>
+    </figure>
+  );
+};
+
+export default MinCard;
